47dc92a532c8aa430e8616f98e1c9aba1a4c229e
Codefarmer <abadasamuelosp@gmail.com>
2025-08-16 08:10:51 +0100

    🧪 (chore): add failing test for no_private_properties_in_public_api

---
+++ test/no_private_properties_in_public_api_test.dart
@@ -0,0 +1,54 @@
+import 'package:analyzer/src/lint/registry.dart';
+import 'package:analyzer_testing/analysis_rule/analysis_rule.dart';
+import 'package:gotham_lints/src/rules/no_private_properties_in_public_api.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(NoPrivatePropertiesInPublicApiRuleTest);
+  });
+}
+
+@reflectiveTest
+class NoPrivatePropertiesInPublicApiRuleTest extends AnalysisRuleTest {
+  @override
+  String get analysisRule => 'no_private_properties_in_public_api';
+
+  @override
+  void setUp() {
+    Registry.ruleRegistry.registerLintRule(NoPrivatePropertiesInPublicApiRule());
+    super.setUp();
+  }
+
+  Future<void> test_public_method_with_private_return() async {
+    await assertDiagnostics(
+      r'''
+class MyClass {
+  _PrivateType getPrivate() => _PrivateType();
+}
+class _PrivateType {}
+''',
+      [lint(18, 12)], // Character 18 and spans 12 characters
+    );
+  }
+
+  Future<void> test_public_method_with_private_parameter() async {
+    await assertDiagnostics(
+      r'''
+class MyClass {
+  void takePrivate(_PrivateType value) {}
+}
+class _PrivateType {}
+''',
+      [lint(32, 13)],
+    );
+  }
+
+  Future<void> test_public_method_with_public_types() async {
+    await assertNoDiagnostics(r'''
+class MyClass {
+  void takePublic(String value) {}
+}
+''');
+  }
+}
